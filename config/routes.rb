bc599bb81dd27201c963d77e74fd6860686a2068
Mandil Khadka <khadkamandil62@gmail.com>
2025-08-12 14:22:10 +0900

    half dones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
 
+  get "/tasks", to: "tasks#index", as: :tasks
+
+  get "/tasks/new", to: "tasks#new", as: :new_list
+
+  post "tasks", to: "tasks#create"
+
+  get "/tasks/:id", to: "tasks#show", as: :task
+
   
 end
